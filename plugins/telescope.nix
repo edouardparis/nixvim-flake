c388fa61ea91658ee77df8becf89f09bc0b12ad3
edouardparis <m@edouard.paris>
2024-09-26 17:40:57 +0200

    fix plugins

--- plugins/telescope.nix
+++ plugins/telescope.nix
@@ -1,28 +1,25 @@
 {
   plugins.telescope = {
     enable = true;
+    keymaps = {
+      # Find files using Telescope command-line sugar.
+      "<leader>ff" = { action = "find_files"; };
+      "<leader>fg" = { action = "live_grep"; };
+      "<leader>b" = { action = "buffers";  };
+      "<leader>fh" = { action = "help_tags"; };
+      "<leader>fd" = { action = "diagnostics"; };
+    };
 
-    settings = {
-      keymaps = {
-        # Find files using Telescope command-line sugar.
-        "<leader>ff" = { action = "find_files"; option.silent = true; };
-        "<leader>fg" = { action = "live_grep"; option.silent = true; };
-        "<leader>b" = { action = "buffers"; option.silent = true; };
-        "<leader>fh" = { action = "help_tags"; option.silent = true; };
-        "<leader>fd" = { action = "diagnostics"; option.silent = true; };
-      };
-
-      defaults = {
-        file_ignore_patterns = [
-          "^.git/"
-            "^.mypy_cache/"
-            "^__pycache__/"
-            "^output/"
-            "^data/"
-            "%.ipynb"
-        ];
-        # set_env.COLORTERM = "truecolor";
-      };
+    settings.defaults = {
+      file_ignore_patterns = [
+        "^.git/"
+        "^.mypy_cache/"
+        "^__pycache__/"
+        "^output/"
+        "^data/"
+        "%.ipynb"
+      ];
+      # set_env.COLORTERM = "truecolor";
     };
   };
 }
